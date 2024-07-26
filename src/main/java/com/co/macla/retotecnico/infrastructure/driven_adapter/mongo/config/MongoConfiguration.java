9c15156633142ab5f9f81d50fd27ce237119ca3b
santiagomac <smazuerac@unac.edu.co>
2024-07-25 23:51:00 -0500

    configuration of mongo

--- src/main/java/com/co/macla/retotecnico/infrastructure/driven_adapter/mongo/config/MongoConfiguration.java
+++ src/main/java/com/co/macla/retotecnico/infrastructure/driven_adapter/mongo/config/MongoConfiguration.java
@@ -1,2 +1,28 @@
-package com.co.macla.retotecnico.infrastructure.driven_adapter.mongo.config;public class MongoConfiguration {
+package com.co.macla.retotecnico.infrastructure.driven_adapter.mongo.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.mongo.MongoConnectionDetails;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.boot.autoconfigure.mongo.PropertiesMongoConnectionDetails;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MongoConfiguration {
+
+    @Bean
+    public MongoDBSecret dbSecret(@Value("${spring.data.mongo.uri}") String uri) {
+        return MongoDBSecret.builder()
+                .uri(uri)
+                .build();
+    }
+
+
+    @Bean
+    public MongoConnectionDetails mongoProperties(MongoDBSecret secret) {
+        MongoProperties properties = new MongoProperties();
+        properties.setUri(secret.getUri());
+        return new PropertiesMongoConnectionDetails(properties);
+
+    }
 }
